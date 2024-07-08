d6d948d213ea4971e8136d50199d6c7c9b4868d0
Charis code <chrx848@gmail.com>
2024-07-08 12:49:44 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+// const slides = document.querySelector('.shoe-type');
+// const images = document.querySelectorAll('.shoe-type img');
+// const totalImages = images.length;
+// let currentIndex = 0;
+
+// document.querySelector('.fa-solid fa-chevron-right').addEventListener('click', showNextImage);
+// document.querySelector('.fa-solid fa-chevron-left').addEventListener('click', showPreviousImage);
+
+// function showNextImage() {
+//     currentIndex = (currentIndex + 1) % totalImages;
+//     slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+// }
+
+// function showPreviousImage() {
+//     currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+//     slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+// }
